dd77cc3f4f050d646e75f01aeba28184be625ddc
josefmonje <josefmonje@gmail.com>
2018-01-29 14:03:04 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+// Rollup plugins
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
+import css from 'rollup-plugin-css-porter';
+import livereload from 'rollup-plugin-livereload';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -18,30 +21,28 @@ export default {
     svelte({
       // enable run-time checks when not in production
       dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
+      // extract component CSS to a separate file
       css: css => {
         css.write('public/static/css/bundle.css');
-    },
-
+      },
       // enable https://svelte.technology/guide#state-management
       store: true,
-
       // this results in smaller CSS files
       cascade: false
     }),
 
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration —
-    // consult the documentation for details:
     // https://github.com/rollup/rollup-plugin-commonjs
     resolve(),
     commonjs(),
 
-    // If we're building for production (npm run build
-    // instead of npm run dev), transpile and minify
+    // production?
     production && buble({ exclude: 'node_modules/**' }),
-    production && uglify()
+    production && uglify(),
+    css({
+      minified: true,
+      dest: 'public/static/css/styles.css',
+    }),
+
+    production ? false : livereload('public'),
   ]
 };
